39940b64a6e5bd09915cd84e326c3af7b96c4ad9
kishan-kumar-ji <158287711+kishan-kumar-ji@users.noreply.github.com>
2024-04-15 17:06:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     APP_ID = int(os.environ.get("APP_ID",'25252648'))
     API_HASH = os.environ.get("API_HASH",'17a1e3c7f59dd46196f0570f9df34955')
     BOT_TOKEN = os.environ.get("BOT_TOKEN",'7199477809:AAHP3l7vMe_U2LiwQNc657mzqLo4f5re-f0')
-    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL",'https://t.me/+Zee3zbllPhw5MTk0'))
+    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL",'-1002032506013'))
     MONGODB_URL = os.environ.get("MONGODB_URL",'mongodb+srv://alienufowala:kxzen@cluster0.ahif6tl.mongodb.net/')
     BOT_OWNER = int(os.environ.get("BOT_OWNER",'6986912824'))
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/IDNCoderXRoot"
